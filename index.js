3f62ad84cc75d81e6615167dea66b4e9001ae5b5
Arun Kumar <arunkumarm1995@gmail.com>
2016-08-10 10:24:17 +0530

    Add a new route and view for new movie form

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var express = require('express'),
     mongoClient = require('mongodb').MongoClient,
-    app = express();
+    app = express(),
+    range = require('node-range');
 
+app.set('view engine', 'pug'); // Set templating engine
 // Request handlers
 
 app.get('/', function(req, res){
-  res.send('Hey, there!');
+  res.render('index');
+});
+
+app.get('/new', function(req, res){
+
+  // Pass years for form field options
+  initialYear = 1913;
+  currentYear = new Date().getFullYear();
+  years = range(initialYear, currentYear + 1).toArray();
+
+  res.render('new', { years: years });
 });
 
 app.use(function(req, res){
